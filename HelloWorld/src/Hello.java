b85ccd71d7669a80b6d7f4edd76568a97cc944f0
jiazxu <jessiepoorxu@gmail.com>
2017-03-08 14:34:03 -0500

    Change 2

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World,  3/8/17");
+		System.out.print("Hello World,  Dance");
 	}
 }
